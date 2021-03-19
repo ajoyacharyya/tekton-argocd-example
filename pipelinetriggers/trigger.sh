99c05111a6586c913849f28c088dd61a1644bb45
Ajoy Acharyya <ajoyacharyya@Ajoys-MacBook-Pro.local>
2021-03-19 14:54:51 +0530

    pipeline trigger changes

--- pipelinetriggers/trigger.sh
+++ pipelinetriggers/trigger.sh
@@ -4,7 +4,7 @@
 
 curl -i \
   -H 'X-GitHub-Event: push' \
-  -H 'X-Hub-Signature: sha1=<...>' \
+  -H 'X-Hub-Signature: sha1=MySecret123' \
   -H 'Content-Type: application/json' \
   -d '{"ref":"refs/heads/main","head_commit":{"id":"d9c7ccb0713da882d8373169e2a1d705f5959386"}}' \
-  http://tekton-triggers.example.com
+  http://devready.cp4multicloud-ibm.com/el-github-listener-interceptor
